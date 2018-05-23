56209d60480ee87ceaa3ed583f903f30d8f60a71
Calascibetta Romain <romain.calascibetta@gmail.com>
2018-05-23 14:33:20 +0200

    Fix segfault

--- src-c/native/stubs.c
+++ src-c/native/stubs.c
@@ -1,19 +1,21 @@
 #include "checkseum.h"
 #include "adler32.h"
 
-#ifdef ARCH_SISTYFOUR
+#include <stdio.h>
+
+#ifdef ARCH_SIXTYFOUR
 /* XXX(dinosaure): un-allocated version for 64-bits architecture. */
 
 CAMLprim value
 caml_checkseum_adler32_ba(value t, value src, value off, value len)
 {
-  return (Val_int (adler32(Int32_val (t), _ba_uint8_off (src, off), Int_val (len))));
+  return (Val_int (adler32(Int_val (t), _ba_uint8_off (src, off), Int_val (len))));
 }
 
 CAMLprim value
 caml_checkseum_adler32_st(value t, value src, value off, value len)
 {
-  return (Val_int (adler32(Int32_val (t), _st_uint8_off (src, off), Int_val (len))));
+  return (Val_int (adler32(Int_val (t), _st_uint8_off (src, off), Int_val (len))));
 }
 
 #else
