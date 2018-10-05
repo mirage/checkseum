dfb243640704e0a9c70e2b1f6b6e847ce982da5c
Calascibetta Romain <romain.calascibetta@gmail.com>
2018-10-05 15:07:49 +0200

    ocamlformat pass

--- src-ocaml/checkseum.ml
+++ src-ocaml/checkseum.ml
@@ -1,18 +1,18 @@
-type bigstring = (char, Bigarray.int8_unsigned_elt, Bigarray.c_layout) Bigarray.Array1.t
+type bigstring =
+  (char, Bigarray.int8_unsigned_elt, Bigarray.c_layout) Bigarray.Array1.t
 
-module type S =
-sig
+module type S = sig
   type t = Optint.t
 
-  val pp: Format.formatter -> t -> unit
-  val equal: t -> t -> bool
-  val default: t
-  val digest_bytes: Bytes.t -> int -> int -> t -> t
-  val digest_string: String.t -> int -> int -> t -> t
-  val digest_bigstring: bigstring -> int -> int -> t -> t
+  val pp : Format.formatter -> t -> unit
+  val equal : t -> t -> bool
+  val default : t
+  val digest_bytes : Bytes.t -> int -> int -> t -> t
+  val digest_string : String.t -> int -> int -> t -> t
+  val digest_bigstring : bigstring -> int -> int -> t -> t
 end
 
-module Adler32: S = struct
+module Adler32 : S = struct
   type t = Optint.t
 
   let equal = Optint.equal
@@ -23,7 +23,7 @@ module Adler32: S = struct
   let digest_bigstring = Gin_adler32.digest_bigstring
 end
 
-module Crc32c: S = struct
+module Crc32c : S = struct
   type t = Optint.t
 
   let equal = Optint.equal
