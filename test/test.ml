38f954efc2f500fe1402f26d30b423f8b66c676c
Romain Calascibetta <romain.calascibetta@gmail.com>
2019-09-11 12:43:00 +0200

    Update tests

--- test/test.ml
+++ test/test.ml
@@ -16,26 +16,26 @@ let () =
             "\x00" (Optint.of_int 0x527d5351)
         ; make ~name:"2"
             (module Checkseum.Crc32c)
-            "\xff\xff\xff\xff" (Optint.of_int 0xffffffff)
+            "\xff\xff\xff\xff" (Optint.of_int32 0xffffffffl)
         ; make ~name:"3"
             (module Checkseum.Crc32c)
-            "123456789" (Optint.of_int 0xe3069283)
+            "123456789" (Optint.of_int32 0xe3069283l)
         ; make ~name:"4"
             (module Checkseum.Crc32c)
             "Thou hast made me, and shall thy work decay?"
-            (Optint.of_int 0x866374c0) ] )
+            (Optint.of_int32 0x866374c0l) ] )
     ; ( "crc32"
       , [ make ~name:"0" (module Checkseum.Crc32) "" Optint.zero
         ; make ~name:"1"
             (module Checkseum.Crc32)
-            "\x00" (Optint.of_int 0xd202ef8d)
+            "\x00" (Optint.of_int32 0xd202ef8dl)
         ; make ~name:"2"
             (module Checkseum.Crc32)
-            "\xff\xff\xff\xff" (Optint.of_int 0xffffffff)
+            "\xff\xff\xff\xff" (Optint.of_int32 0xffffffffl)
         ; make ~name:"3"
             (module Checkseum.Crc32)
-            "123456789" (Optint.of_int 0xCBF43926)
+            "123456789" (Optint.of_int32 0xcbf43926l)
         ; make ~name:"4"
             (module Checkseum.Crc32)
             "Thou hast made me, and shall thy work decay?"
-            (Optint.of_int 0xF1FABE1D) ] ) ]
+            (Optint.of_int32 0xf1fabe1dl) ] ) ]
