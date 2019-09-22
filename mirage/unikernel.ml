8e90d0c9de162a055ec0de9cd610c9b80aafe2e2
Calascibetta Romain <romain.calascibetta@gmail.com>
2019-09-22 02:26:00 +0200

    Revert "Revert variants"
    This reverts commit 04efbed60f63e8107937d5b9357f7e92b57ae2c1.

--- mirage/unikernel.ml
+++
@@ -1,8 +0,0 @@
-module Make (Console : Mirage_types_lwt.CONSOLE)
-= struct
-  let log console fmt = Fmt.kstrf (Console.log console) fmt
-
-  let start console =
-    let c = Checkseum.Adler32.digest_string "" 0 0 Checkseum.Adler32.default in
-    log console "%a%!" Checkseum.Adler32.pp c
-end
